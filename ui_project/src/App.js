b2a6d219ebf5ad1ce2bc6f326f3f6adf898b7493
Venkat CH <ch.venkat668@hotmail.com>
2024-12-15 20:12:59 +0530

    setting up the react

---
+++ ui_project/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from "./HelloWorld/HelloWorld"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> welcome to react </h1>
+        <HelloWorld />
+        <HelloWorld />
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui_project/src/HelloWorld/HelloWorld.js
@@ -0,0 +1,18 @@
+
+export default function HelloWorld(){
+    return(
+        <div>
+            <h1> HelloWorld </h1>
+            <p> lorem ipusm is simply a dummy text</p>
+            <ul>
+                <li> list 1</li>
+                <li> list 1</li>
+                <li> list 1</li>
+                <li> list 1</li>
+                <li> list 1</li>
+                <li> list 1</li>
+                
+            </ul>
+        </div>
+    )
+}
